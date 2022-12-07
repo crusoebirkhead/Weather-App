7f7fcaf52860a16864316180d4a79c2b7503075f
crusoebirkhead <crusoebirkhead@gmail.com>
2022-12-07 00:04:09 -0800

    m

--- src/printData.js
+++ src/printData.js
@@ -1,9 +1,26 @@
 import { weatherRequest } from "./weatherRequest"
 
 function printData(data){
-    let base = document.createElement('p')
-    document.body.appendChild(base)
-    base.innerHTML += data.base
+
+    let name = document.createElement('p')
+    document.body.appendChild(name)
+    name.innerHTML += data.name
+
+    let max = document.createElement('p')
+    document.body.appendChild(max)
+    max.innerHTML += data.main.temp_max
+
+    let min = document.createElement('p')
+    document.body.appendChild(min)
+    min.innerHTML += data.main.temp_min
+
+    let feels = document.createElement('p')
+    document.body.appendChild(feels)
+    feels.innerHTML += data.main.feels_like
+
+    let countryDisplay = document.createElement('p')
+    document.body.appendChild(countryDisplay)
+    countryDisplay.innerHTML += data.sys.country
 
 }
 
